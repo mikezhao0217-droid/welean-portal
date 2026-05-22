1718adaa5f2b7fe6a651b803123142e3919ae0fe
夏目友人 <psoonline@163.com>
2026-05-22 13:23:00 +0800

    fix: add root page re-export to fix 404 on /

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { locales, defaultLocale } from './i18n';
 export default createMiddleware({
   locales,
   defaultLocale,
-  localePrefix: 'as-needed'
+  localePrefix: 'always'
 });
 
 export const config = {
